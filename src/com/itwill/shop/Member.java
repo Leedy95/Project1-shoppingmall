417e9e725f6250a4fc4a69e63d3f30152484238f
lee do young <leedy95@hanmail.net>
2022-01-24 09:49:35 +0900

    ..

--- src/com/itwill/shop/Member.java
+++ src/com/itwill/shop/Member.java
@@ -1,6 +1,21 @@
 package com.itwill.shop;
 
 public class Member {
+	private int no;
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [no=" + no + "]";
+	}
+	
 	
 
 }
